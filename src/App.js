5f4972da9c0e17063fadcc624e0f1edff30efec1
wriozumi <tshatmanaliev@gmail.com>
2019-07-13 13:21:25 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./components/Posts";
+import Postform from "./components/Postform";
+import { Provider } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import store from './store';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
